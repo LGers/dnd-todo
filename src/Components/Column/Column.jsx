1144fb6890817985e9668cd779a94984e1106709
LGers <87089817+LGers@users.noreply.github.com>
2022-05-25 18:34:37 +0300

    feat: drag event. Reorder a List with react-beautiful-dnd

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import { Card, Paper, Typography, Stack, Container } from '@mui/material';
 import { Task } from '../Task/Task';
@@ -5,6 +6,7 @@ import { Task } from '../Task/Task';
 export const Column = (props) => {
   const { title, description, tasks } = props;
   console.log(tasks);
+  const ref = useRef();
 
   return (
     <Card sx={{ width: '250px', bgcolor: '#d5d5ff' }}>
@@ -13,13 +15,28 @@ export const Column = (props) => {
         <Typography variant={'h5'} component={'div'}>{title}</Typography>
         <Typography variant={'h6'} component={'div'}>{description}</Typography>
         <Droppable droppableId={props.column.id}>
-          {() => (
-            <Stack spacing={2}>
-            {tasks.map((task) => {
-                return <Task key={task.id} title={task.title} description={task.description} />
-              }
-            )}
-          </Stack>
+          {(provided) => (
+            // TODO what is inner ref
+            // FIXME what is inner ref
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <Stack
+                spacing={2}
+                // innerRef={provided.innerRef}
+                // {...provided.droppableProps}
+              >
+                {tasks.map((task, index) => {
+                    return <Task
+                      key={task.id}
+                      title={task.title}
+                      description={task.description}
+                      task={task}
+                      index={index}
+                    />
+                  }
+                )}
+                {provided.placeholder}
+              </Stack>
+            </div>
           )}
           {/* <Stack spacing={2}>
             {tasks.map((task) => {
--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -1,12 +1,25 @@
+import { Draggable } from 'react-beautiful-dnd';
 import { Card, Paper, Stack, Typography } from '@mui/material';
 
 export const Task = (props) => {
   return (
-    <Stack spacing={2}>
-      <Paper sx={{ width: '100%' }}>
-        {/* <Typography variant={'h4'} component={'div'}>Column</Typography> */}
-        <Typography variant={'h6'} component={'div'}>{props.title}</Typography>
-        <Typography component={'div'}>{props.description}</Typography>
-      </Paper>
-    </Stack>
+    <Draggable
+      draggableId={props.task.id}
+      index={props.index}
+    >
+      {(provided) => (
+        <Stack spacing={2}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <Paper sx={{ width: '100%' }}>
+            {/* <Typography variant={'h4'} component={'div'}>Column</Typography> */}
+            <Typography variant={'h6'} component={'div'}>{props.title}</Typography>
+            <Typography component={'div'}>{props.description}</Typography>
+          </Paper>
+        </Stack>
+        // {provided.pl}
+      )}
+    </Draggable>
 )};
